807727123bd1c579c912a3a00b9e2b66cbd7570b
lior-benjman <lior114452@gmail.com>
2025-02-26 11:16:47 +0200

    prog

---
+++ Day 2/variables.py
@@ -0,0 +1,48 @@
+#Day 2: 30 Days of python programming
+first_name = "Lior"
+last_name = "Benjman"
+fullname = "Lior Benjman"
+country = "Israel"
+city = "Lod"
+age = 24
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
+cloth,color = "jacket","blue"
+print(type(first_name))
+print(type(last_name))
+print(type(fullname))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+_total = num_one + num_two
+_diff = num_one - num_two
+_product = num_one * num_two
+_division = num_one / num_two
+_remainder = num_one % num_two
+_exp = num_one ** num_two
+_floor_division = num_one // num_two
+
+radius = 30
+area_of_circle = 3.14*(radius**2)
+
+r = float(input("Enter the radius "))
+area = 3.14*(r**2)
+
+firstname = input("Enter firstname ")
+lastname = input("Enter lastname ")
+country = input("Enter country ")
+age = input("Enter age ")
